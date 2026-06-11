ebf5b8e50b1d9759bcfb7f5a172e4ed443cdc750
Soliprem <franci.solidoro@gmail.com>
2026-06-12 00:59:41 +0200

    watt: init at 1.2.0

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -26053,6 +26053,13 @@
     githubId = 7116239;
     keys = [ { fingerprint = "E067 520F 5EF2 C175 3F60  50C0 BA46 725F 6A26 7442"; } ];
   };
+  Soliprem = {
+    email = "me@soliprem.eu";
+    matrix = "@soliprem:soliprem.eu";
+    name = "Francesco Prem Solidoro";
+    github = "Soliprem";
+    githubId = 73885403;
+  };
   solson = {
     email = "scott@solson.me";
     matrix = "@solson:matrix.org";
